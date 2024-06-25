3ff9d7a35cceba6718e0d87110a31bbe2d84a908
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-06-25 20:39:38 +0500

    Modified ProfileCommands.java to return boolean value

--- src/main/java/com/ahmedxmujtaba/Commands/ProfileCommands.java
+++ src/main/java/com/ahmedxmujtaba/Commands/ProfileCommands.java
@@ -1,6 +1,9 @@
 package com.ahmedxmujtaba.Commands;
 
 import com.ahmedxmujtaba.Backend.Entities.Profile;
+import com.ahmedxmujtaba.Backend.Utility.Date;
+
+import java.time.LocalDate;
 
 public class ProfileCommands implements Command {
 private Profile profile;
@@ -8,7 +11,7 @@ private Profile profile;
     this.profile = profile;
 }
     @Override
-    public void execute(String[] arguments) {
+    public boolean execute(String[] arguments) {
         if (arguments.length > 0) {
             String subCommand = arguments[0];
             switch (subCommand) {
@@ -18,64 +21,71 @@ private Profile profile;
                         switch (paramToChange) {
                             case "name", "n":
                                 // call function to change name
-                                changeName(arguments[2]);
-                                break;
+                                return changeName(arguments[2]);
                             case "dob":
                                 // function to change dob
-                                changeDOB(arguments[2]);
-                                break;
+                                return changeDOB(arguments[2]);
                             case "class", "c":
                                 // function to change class
-                                changeClass(arguments[2]);
-                                break;
+                                return changeClass(arguments[2]);
                             case "description", "d":
                                 // function to change description
-                                changeDescription(arguments[2]);
-                                break;
+                                return changeDescription(arguments[2]);
                             default:
                                 System.out.println("Invalid Command: Profile -[field] [value] \n Profile -help to see options");
-                                break;
+                                return false;
                         }
                     }
                     else {
                         System.out.println("Invalid Command: Profile -[field] [value] \n Profile -help to see options");
+                        return false;
                     }
-                    break;
                 case "-view", "-display", "v":
                     // display profile
                     displayProfile();
-                    break;
+                    return false; //no need to save any data since none is updated
                 case "-help", "-h":
                     // display help
                     displayHelp();
-                    break;
+                    return false;
                 default:
                     System.out.println("Invalid Command: Profile -[field] \n-help to see options");
-                    break;
+                    return false;
             }
         } else {
             System.out.println("Usage: Profile <field> [options]");
+            return false;
         }
     }
 
-    private void changeName(String newName) {
+    private boolean changeName(String newName) {
         profile.setName(newName);
         System.out.println("Name changed to: " + newName);
+        return true;
     }
 
-    private void changeDOB(String newDOB) {
-
-        System.out.println("DOB changed to: " + newDOB);
+    private boolean changeDOB(String inputDate) {
+        if (Date.isValidDate(inputDate)) {
+            LocalDate date = Date.parseDate(inputDate);
+            String formattedDate = Date.formatDate(date);
+            profile.setDOB(formattedDate);
+            System.out.println("Date of Birth changed to: " + formattedDate);
+            return true;
+        } else {
+            System.out.println("Invalid date format. \nPlease enter date as dd/MM/yyyy");
+            return false;
+        }
     }
-
-    private void changeClass(String newClass) {
-        // logic to change the class
+    private boolean changeClass(String newClass) {
+        profile.set_class_(newClass);
         System.out.println("Class changed to: " + newClass);
+        return true;
     }
 
-    private void changeDescription(String newDescription) {
-        // logic to change the description
+    private boolean changeDescription(String newDescription) {
+        profile.setDescription(newDescription);
         System.out.println("Description changed to: " + newDescription);
+        return true;
     }
 
     private void displayHelp() {
