0fc0e4f11a02abdd55734011bf44180d9d844382
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-06-25 20:40:57 +0500

    Modified Main

--- src/main/java/com/ahmedxmujtaba/Main.java
+++ src/main/java/com/ahmedxmujtaba/Main.java
@@ -1,7 +1,32 @@
 package com.ahmedxmujtaba;
 
-public class Main {
-    public static void main(String[] args) {
+import com.ahmedxmujtaba.Backend.IO.IOManager;
+import com.ahmedxmujtaba.Commands.CommandManager;
+import com.ahmedxmujtaba.Commands.LoadCommands;
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        input();
     }
+    public static void input(){
+        CommandManager cmd = LoadCommands.getCmd();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter commands (type 'exit' to quit):");
+
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            if(cmd.executeCommand(input)){
+                //todo add function to save file
+            };
+
+        }
+    }
+
 }
