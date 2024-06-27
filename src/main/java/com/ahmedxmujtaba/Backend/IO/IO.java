0074de17efc7bfc7f3be9dd3d7a8ffe506702d7f
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-06-27 17:45:35 +0500

    Modified to load and save files from Data dir

--- src/main/java/com/ahmedxmujtaba/Backend/IO/IO.java
+++ src/main/java/com/ahmedxmujtaba/Backend/IO/IO.java
@@ -1,39 +1,58 @@
 package com.ahmedxmujtaba.Backend.IO;
 
-import javax.management.ObjectInstance;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class IO {
 
     // Save the object to the specified file location
-    public static boolean saveFile (String fileLocation, Object object) {
+    public static boolean saveFile(String fileLocation, Object object) {
         try (FileOutputStream fileOutputStream = new FileOutputStream(fileLocation);
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
             objectOutputStream.writeObject(object);
             return true;
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.err.println("File not found: " + e.getMessage());
+            return false;
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
+            System.err.println("IO exception: " + e.getMessage());
             return false;
         }
     }
 
-    // Load the object from the specified file location
-    public static Object loadFile (String fileLocation) {
+    // Load the object from the specified file location with a generic type
+    @SuppressWarnings("unchecked")
+    public static <T> T loadFile(String fileLocation) {
         try (FileInputStream fileInputStream = new FileInputStream(fileLocation);
              ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
-            return objectInputStream.readObject();
+            return (T) objectInputStream.readObject();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Generating new file at dir: " + fileLocation );
+            return null;
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
+            System.err.println("Class not found: " + e.getMessage());
             return null;
         }
     }
-}
 
+    // Create a folder if it does not exist
+    public static void createFolderIfNotExists(String folderPath) {
+        Path path = Paths.get(folderPath);
+        if (Files.notExists(path)) {
+            try {
+                Files.createDirectories(path);
+                System.out.println("Folder created at: " + path);
+            } catch (IOException e) {
+                System.err.println("Failed to create folder: " + e.getMessage());
+            }
+        }
+    }
+
+    // Get the name of the current directory where the program is being executed
+    public static String getCurrentDirectory() {
+        Path currentDir = Paths.get("");
+        return currentDir.toAbsolutePath().toString();
+    }
+}
--- src/main/java/com/ahmedxmujtaba/Backend/IO/IOManager.java
+++ src/main/java/com/ahmedxmujtaba/Backend/IO/IOManager.java
@@ -2,7 +2,7 @@ package com.ahmedxmujtaba.Backend.IO;
 
 import com.ahmedxmujtaba.Backend.Entities.Profile;
 import com.ahmedxmujtaba.Backend.Entities.SkillTree;
-import com.ahmedxmujtaba.Backend.Entities.Task;
+import com.ahmedxmujtaba.Backend.Entities.TaskList;
 
 public class IOManager {
     //todo make the following files:
@@ -12,23 +12,61 @@ public class IOManager {
     // skill tree
     // quests
 
-    private static String taskFileName = "Tasks.ser";
-    private static String dailyTasksFileName = "Daily_Tasks.ser";
-    private static String profileFileName = "Profile.ser";
-    private static String skillTreeFileName = "Skill_Tree.ser";
-    private static String questFileName = "Quests.ser";
+    private static String folderDir = "../Data";
+    private static String taskFileDir = folderDir+"/Task_List.ser";
+    private static String dailyTasksFileDir = folderDir+"/Daily_Tasks.ser";
+    private static String profileFileDir = folderDir+"/Profile.ser";
+    private static String skillTreeFileDir = folderDir+"/Skill_Tree.ser";
+    private static String questFileDir = folderDir+"/Quests.ser";
+
 
     //save Tasks
-    public static boolean saveTasks(Task task){
-        if (IO.saveFile(taskFileName,task)) return true;
+    public static boolean saveTasks(TaskList taskList){
+        if (IO.saveFile(taskFileDir,taskList)) return true;
         else return false;
     }
     public static boolean saveProfile(Profile profile){
-        if (IO.saveFile(profileFileName,profile)) return true;
+        if (IO.saveFile(profileFileDir,profile)) return true;
         else return false;
     }
     public static boolean saveSkillTree(SkillTree skillTree){
-        if (IO.saveFile(skillTreeFileName,skillTree)) return true;
+        if (IO.saveFile(skillTreeFileDir,skillTree)) return true;
         else return false;
     }
+    public static SkillTree loadSkillTree(){
+        SkillTree skillTree = IO.loadFile(skillTreeFileDir);
+        if (skillTree != null) return skillTree;
+        //if no skill tree exists return new since first program runs
+
+        else {
+            SkillTree newSkillTree = new SkillTree();
+            if(saveSkillTree(newSkillTree))System.out.println("Skill_Tree.ser Created");
+            return newSkillTree;
+        }
+    }
+
+    public static Profile loadProfile(){
+        Profile profile = IO.loadFile(profileFileDir);
+        if (profile != null) return profile;
+        else
+        {
+            Profile newProfile = new Profile("Struggler");
+            if(saveProfile(newProfile)) System.out.println("Profile.ser Created");
+            return newProfile;
+        }
+    }
+    public static TaskList loadTasks(){
+        TaskList taskList = IO.loadFile(taskFileDir);
+        if (taskList != null) return taskList;
+        else
+        {
+            TaskList newTaskList = new TaskList();
+            if(saveTasks(newTaskList))System.out.println("Task_List.ser Created");
+            return newTaskList;
+        }
+    }
+    public static void makeDataDir(){
+        IO.createFolderIfNotExists(folderDir);
+    }
+
 }
