0cf8a727dbbd00ab276691e8dcf9dc1c4ff80723
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-06-27 17:49:39 +0500

    Modified LoadCommands.commandLoader to check for Data directory and added command Task

--- src/main/java/com/ahmedxmujtaba/Commands/LoadCommands.java
+++ src/main/java/com/ahmedxmujtaba/Commands/LoadCommands.java
@@ -1,18 +1,26 @@
 package com.ahmedxmujtaba.Commands;
 
 import com.ahmedxmujtaba.Backend.Entities.Profile;
+import com.ahmedxmujtaba.Backend.Entities.Skill;
 import com.ahmedxmujtaba.Backend.Entities.SkillTree;
+import com.ahmedxmujtaba.Backend.Entities.TaskList;
+import com.ahmedxmujtaba.Backend.IO.IOManager;
+
+import java.util.ArrayList;
 
 
 public class LoadCommands {
-    private static CommandManager cmd =  new CommandManager();;
+    private static CommandManager cmd =  new CommandManager();
     public LoadCommands() {}
 
     public static void commandLoader(){
-            //todo load profile from file
-            cmd.registerCommand("Profile", new ProfileCommands(new Profile("ahmed")));
-            cmd.registerCommand("Skills", new SkillCommands(new SkillTree()));
-
+            IOManager.makeDataDir();
+            SkillTree skillTree = IOManager.loadSkillTree();
+            Profile profile = IOManager.loadProfile();
+            TaskList taskList = IOManager.loadTasks();
+            cmd.registerCommand("Profile", new ProfileCommands(profile));
+            cmd.registerCommand("Skill", new SkillCommands(skillTree));
+            cmd.registerCommand("Task", new TaskCommands(skillTree,taskList));
     }
 
     public static CommandManager getCmd() {
