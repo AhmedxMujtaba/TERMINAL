450dde9408e85081c3164814f646a65a17815f99
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-07-09 09:54:54 +0500

    Removed redundant code

--- src/main/java/com/ahmedxmujtaba/Commands/Command.java
+++ src/main/java/com/ahmedxmujtaba/Commands/Command.java
@@ -1,20 +1,5 @@
 package com.ahmedxmujtaba.Commands;
 
-
-import java.util.Stack;
-
 public interface Command {
-
-//    private static final String[] profile = {"P","Profile"};
-//    private static final String[] skillTree = {"Skill","Skills","S"};
-//    private static final String[] tasks = {"Task","Tasks","T"};
-//    private static final String[] edit = {"Edit","E"};
-//    private static final String[] add = {"Add","A","Create","C"};
-//    private static final String[] delete = {"Delete","De","Remove","R"};
-//    private static final String[] display = {"Display", "Dis"};
-//    private static final String[] details = {"Display", "Dis"};
-//    private static final String[] markAsDone = {"Done", "Complete", "C"};
-//    private Stack<String> previousCommands;
-
-    public int execute(String[] arguments);
+    int execute(String[] arguments);
 }
